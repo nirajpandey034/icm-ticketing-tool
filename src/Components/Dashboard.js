2dea8b70393d72efb0414a6b80269e8b3d973b79
Niraj Pandey <nirajpandey034@gmail.com>
2021-07-21 09:08:43 +0530

    second commit

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
@@ -6,6 +6,8 @@ import AddIcon from '@material-ui/icons/Add';
 import TicketModal from './TicketModal'
 
 import TicketCards from './TicketCards';
+
+import axios from 'axios'
 
 
 
@@ -21,7 +23,22 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Dashboard() {
   const [openModal, setOpenModal] = useState(false);
+  const [cards, setCards] = useState([]);
   const classes = useStyles();
+
+  useEffect(()=>{
+    axios.get('https://icm-ticketing-tool.herokuapp.com/getAllTickets')
+    .then((response)=>{
+      let res=[];
+      response.data.forEach((card)=>{
+        res = [...res, <TicketCards key={card._id} id={card._id} title={card.title} description={card.description}
+        severity={card.severity} priority={card.priority} raised_timestamp={card.raised_timestamp} 
+        raised_by={card.raised_by} assigned_to={card.assigned_to} state={card.state} resolution={card.resolution}/>]
+      })
+      setCards(res);
+    })
+    .catch((err)=>{console.log(err)});
+  },[])
 
   const openModalHandler = () => {
     setOpenModal(!openModal);
@@ -36,7 +53,7 @@ export default function Dashboard() {
   return (
     <div>
       <h2>Press F9 or the + button.</h2>
-      <TicketCards />
+      {cards}
       <TicketModal openModal={openModal} closeModal={() => { setOpenModal(!openModal) }} />
       <div className={classes.root}>
         <Fab size="medium" color="primary" aria-label="create ticket">
